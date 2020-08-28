c72da259a5e5c24467ef786db521201159f325d3
Flareonn <35931685+Flareonn@users.noreply.github.com>
2020-08-28 12:11:57 +0500

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,5 @@
-function init() {
-  $(".testimonials-slider").slick({
-    infinite: true,
-    arrows: false,
-    dots: true,
-  });
-}
-
-setTimeout(init, 3000);
+$(".testimonials-slider").slick({
+  infinite: true,
+  arrows: false,
+  dots: true,
+});
